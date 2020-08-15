4f35de5a355c0988f092db1ee8c9252c6102a00e
mbernardes19 <bernardes.matheus@outlook.com>
2020-08-15 00:28:39 -0300

    Adds Plano Scene

--- src/scenes/PlanoScene.ts
+++ src/scenes/PlanoScene.ts
@@ -1,9 +1,81 @@
-import { BaseScene } from 'telegraf';
+import { BaseScene, Markup, Extra } from 'telegraf';
+import CacheService from '../services/cache';
+import { log } from '../logger';
+import { silver, gold, diamond, blackDiamond } from '../services/validate';
 
-const wizard = new BaseScene('plano')
-wizard.enter((ctx) => {
-    ctx.reply('Olá!')
-    ctx.scene.enter('pegarinfo')
+const planoScene = new BaseScene('plano')
+
+planoScene.action('silver', async (ctx) => {
+    await ctx.answerCbQuery();
+    await savePlano('silver');
+    await askForFullName(ctx)
+    await ctx.scene.enter('name');
 })
 
-export default wizard;
+planoScene.action('gold', async (ctx) => {
+    await ctx.answerCbQuery();
+    await savePlano('gold');
+    await askForFullName(ctx)
+    await ctx.scene.enter('name');
+})
+
+planoScene.action('diamond', async (ctx) => {
+    await ctx.answerCbQuery();
+    await savePlano('diamond');
+    await askForFullName(ctx)
+    await ctx.scene.enter('name');
+})
+
+planoScene.action('black_diamond', async (ctx) => {
+    await ctx.answerCbQuery();
+    await savePlano('black_diamond');
+    await askForFullName(ctx)
+    await ctx.scene.enter('name');
+})
+
+planoScene.use(async (ctx) => {
+    if (silver(ctx)) {
+        if (!ctx.message) {
+            await ctx.answerCbQuery()
+        }
+        await savePlano('silver');
+        await askForFullName(ctx);
+        await ctx.scene.enter('name');
+    }
+    if (gold(ctx)) {
+        if (!ctx.message) {
+            await ctx.answerCbQuery()
+        }
+        await savePlano('gold');
+        await askForFullName(ctx);
+        await ctx.scene.enter('name');
+    }
+    if (diamond(ctx)) {
+        if (!ctx.message) {
+            await ctx.answerCbQuery()
+        }
+        await savePlano('diamond');
+        await askForFullName(ctx);
+        await ctx.scene.enter('name');
+    }
+    if (blackDiamond(ctx)) {
+        if (!ctx.message) {
+            await ctx.answerCbQuery()
+        }
+        await savePlano('black_diamond');
+        await askForFullName(ctx);
+        await ctx.scene.enter('name');
+    }
+});
+
+const savePlano = async (plano) => {
+    CacheService.saveUserData('plano', plano);
+    log(`Plano definido ${plano}`);
+}
+
+const askForFullName = async (ctx) => {
+    await ctx.reply('Ok!');
+    await ctx.reply('Qual é o seu nome completo?');
+}
+
+export default planoScene;
