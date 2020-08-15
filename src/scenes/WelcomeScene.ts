63858ef7d9fd75a46ca7e80ecd082d050c0591f6
mbernardes19 <bernardes.matheus@outlook.com>
2020-08-15 00:28:47 -0300

    Adds Welcome Scene

--- src/scenes/WelcomeScene.ts
+++ src/scenes/WelcomeScene.ts
@@ -1,20 +1,24 @@
 import { BaseScene, Markup, Extra } from 'telegraf';
-import { log } from '../logger';
-import CacheService from '../services/cache';
 
+const welcomeScene = new BaseScene('welcome')
 
-const wizard = new BaseScene('welcome')
-wizard.enter(async (ctx) => {
-    log('primeira mensagem').info();
-    CacheService.saveUserData(ctx, 'teste', 'oi');
-    await ctx.reply('Olá! Seja bem-vindo!');
-    await ctx.reply('Vou precisar confirmar a sua forma de pagamento!');
+welcomeScene.enter(async (ctx) => {
+    await welcome(ctx);
+    await showPaymentOptions(ctx);
+    await ctx.scene.enter('payment')
+})
+
+const welcome = async (ctx) => {
+    await ctx.reply('Olá, eu sou o Bot do Método Trader Infalível 🤖💵 Estou aqui para te dar acesso aos nossos canais de Telegram para que você possa começar a trilhar seu caminho rumo à riqueza!');
+    await ctx.reply('Preciso primeiramente confirmar no servidor da Monetizze se o seu pagamento já foi aprovado.\n\nPor isso, gostaria de saber algumas informações de você...');
+}
+
+const showPaymentOptions = async (ctx) => {
     const pagamento = Markup.inlineKeyboard([
         [Markup.callbackButton('💳 Cartão de Crédito', 'cartao_de_credito')],
         [Markup.callbackButton('📄 Boleto', 'boleto')]
     ])
-    await ctx.reply("Qual foi sua forma de pagamento", Extra.markup(pagamento))
-    ctx.scene.enter('payment')
-})
+    await ctx.reply("Qual foi sua forma de pagamento?", Extra.markup(pagamento))
+}
 
-export default wizard;
+export default welcomeScene;
