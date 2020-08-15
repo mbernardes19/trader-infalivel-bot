c9f20105ad144d1509948684dcec2e0ab82ccaad
mbernardes19 <bernardes.matheus@outlook.com>
2020-08-15 00:28:03 -0300

    Adds Name Scene

--- src/scenes/NameScene.ts
+++ src/scenes/NameScene.ts
@@ -1,9 +1,49 @@
-import { BaseScene } from 'telegraf';
+import { BaseScene, Extra, Markup } from 'telegraf';
+import CacheService from '../services/cache';
+import { log } from '../logger';
+import { confirmado, negado } from '../services/validate';
 
-const wizard = new BaseScene('name')
-wizard.enter((ctx) => {
-    console.log(    ctx.scene.state)
-    ctx.reply('Olá!')
-})
+const nameScene = new BaseScene('name');
 
-export default wizard;
+nameScene.use(async (ctx) => {
+    await confirmFullName(ctx);
+    await saveFullName(ctx.message.text);
+});
+
+const confirmFullName = async (ctx) => {
+    const confirmacao = Markup.inlineKeyboard([Markup.callbackButton('👍 Sim', 'sim'), Markup.callbackButton('👎 Não', 'nao')])
+    await ctx.reply(`Confirmando... seu nome completo é ${ctx.message.text}?`, Extra.inReplyTo(ctx.update.message.message_id).markup(confirmacao));
+    return ctx.scene.enter('confirm_name');
+}
+
+const saveFullName = async (fullname) => {
+    CacheService.saveUserData('nome_completo', fullname);
+    log(`Nome completo definido ${fullname}`);
+}
+
+const confirmNameScene = new BaseScene('confirm_name');
+
+confirmNameScene.action('sim', async (ctx) => {
+    const nome = CacheService.get<string>('nome_completo');
+    await ctx.reply(`Beleza, ${nome.includes(' ') ? nome.substring(0, nome.indexOf(' ')) : nome}!`);
+    return ctx.scene.enter('phone');
+});
+
+confirmNameScene.action('nao', async (ctx) => {
+    await ctx.reply('Por favor, digite seu nome completo novamente:')
+    return ctx.scene.enter('name');
+});
+
+confirmNameScene.use(async (ctx) => {
+    if (confirmado(ctx)) {
+        const nome = CacheService.get<string>('nome_completo');
+        await ctx.reply(`Beleza, ${nome.includes(' ') ? nome.substring(0, nome.indexOf(' ')) : nome}!`);
+        return ctx.scene.enter('phone');
+    }
+    if (negado(ctx)) {
+        await ctx.reply('Por favor, digite seu nome completo novamente:')
+        return ctx.scene.enter('name');
+    }
+});
+
+export { nameScene, confirmNameScene };
