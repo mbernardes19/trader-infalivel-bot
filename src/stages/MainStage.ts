6bcb2f6bc2dc5652cc51fe8c6b9c498efd50161f
mbernardes19 <bernardes.matheus@outlook.com>
2020-08-15 00:29:45 -0300

    Adjusts MainStage

--- src/stages/MainStage.ts
+++ src/stages/MainStage.ts
@@ -2,11 +2,21 @@ import { Stage } from 'telegraf';
 import welcomeScene from '../scenes/WelcomeScene';
 import paymentScene from '../scenes/PaymentScene';
 import planoScene from '../scenes/PlanoScene';
-import nameScene from '../scenes/NameScene';
-import phoneScene from '../scenes/PhoneScene';
+import { nameScene, confirmNameScene } from '../scenes/NameScene';
+import { phoneScene, confirmPhoneScene } from '../scenes/PhoneScene';
 import emailScene from '../scenes/EmailScene';
 import analysisScene from '../scenes/AnalysisScene';
 
-const stage = new Stage([welcomeScene, paymentScene, planoScene, nameScene, phoneScene, emailScene, analysisScene], { ttl: 1500 });
+const stage = new Stage([
+    welcomeScene,
+    paymentScene,
+    planoScene,
+    nameScene,
+    confirmNameScene,
+    phoneScene,
+    confirmPhoneScene,
+    emailScene,
+    analysisScene
+], { ttl: 1500 });
 
 export default stage;
