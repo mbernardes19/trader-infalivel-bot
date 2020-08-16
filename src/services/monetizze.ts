1d5db382beffcb3a90ca803dff21d45a0013f7b2
mbernardes19 <bernardes.matheus@outlook.com>
2020-08-16 15:59:13 -0300

    Finishes monetizze service

--- src/services/monetizze.ts
+++ src/services/monetizze.ts
@@ -1,23 +1,36 @@
 import { getMonetizzeProductTransaction } from './request';
-import CacheService from './cache';
 import { log } from '../logger';
 
 const getDiscountCouponIdFromUser = async (userEmail, userPlano) => {
-    const transaction = await getMonetizzeProductTransaction({ email: userEmail })
-    const transactionFromPlano = transaction.dados.filter(dado => dado.venda.plano === userPlano);
-    return transactionFromPlano[0].venda.cupom;
+    try {
+        const transaction = await getMonetizzeProductTransaction({ email: userEmail })
+        log(`Pegando cupom de desconto do usuário na Monetizze`);
+        const transactionFromPlano = transaction.dados.filter(dado => dado.venda.plano === userPlano);
+        return transactionFromPlano[0].venda.cupom;
+    } catch (err) {
+        throw err;
+    }
 }
 
-const verifyUserPurchase = async (ctx) => {
-    const email = CacheService.get<string>('email');
+const verifyUserPurchase = async (email) => {
     try {
-        const response = await getMonetizzeProductTransaction({ email, "status[]": [2] })
+        const response = await getMonetizzeProductTransaction({ email, "status[]": 2 })
         log(`Verificando compra de usuário na Monetizze ${JSON.stringify(response)}`)
-        return response.recordCount === "0" ? true : false;
+        return response.recordCount === "0" ? false : true;
     } catch (err) {
         throw err
     }
 }
 
+const checkIfPaymentMethodIsBoleto = async (email) => {
+    try {
+        const response = await getMonetizzeProductTransaction({ email, "forma_pagamento[]": 3, "status[]": 1 })
+        log(`Verificando se a compra na Monetizze foi feita por boleto e está aguardando pagamento ${JSON.stringify(response)}`)
+        return response.recordCount === "0" ? false : true;
+    } catch (err) {
+        throw err;
+    }
+}
 
-export { getDiscountCouponIdFromUser, verifyUserPurchase }
+
+export { getDiscountCouponIdFromUser, verifyUserPurchase, checkIfPaymentMethodIsBoleto }
