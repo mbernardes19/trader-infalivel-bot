632b68f6c23170abdb4dd1a00e27dafab1d2bc41
mbernardes19 <bernardes.matheus@outlook.com>
2020-08-18 23:39:11 -0300

    Updates dao

--- src/dao.ts
+++ src/dao.ts
@@ -1,23 +1,16 @@
 import util from 'util';
-import { Connection, MysqlError } from 'mysql';
+import { Connection } from 'mysql';
 import User from './model/User';
 
 const addUserToDatabase = async (user: User, connection: Connection) => {
     const userData = user.getUserData();
-    const { telegramId, username, fullName, plano, discountCouponId, phone, email, paymentMethod } = userData;
+    const { telegramId, username, fullName, plano, discountCouponId, phone, email, paymentMethod, dataAssinatura } = userData;
     const query = util.promisify(connection.query).bind(connection)
     try {
-        await query(`insert into Users (id_telegram, username_telegram, plano, cupom_desconto, nome_completo, telefone, email, forma_de_pagamento, status_assinatura) values ('${telegramId}', '${username}', '${plano}', '${discountCouponId}', '${fullName}', '${phone}', '${email}', '${paymentMethod}', 'ativa')`)
+        await query(`insert into Users (id_telegram, user_telegram, plano, cupom_desconto, nome_completo, telefone, email, forma_de_pagamento, data_assinatura, status_assinatura) values ('${telegramId}', '${username}', '${plano}', '${discountCouponId}', '${fullName}', '${phone}', '${email}', '${paymentMethod}', '${dataAssinatura}', 'ativa')`)
     } catch (err) {
         throw err
     }
-    // connection.end((err: MysqlError) => {
-    //     if (err) {
-    //         console.log(err)
-    //         return;
-    //     }
-    //     console.log('Conexão finalizada com sucesso')
-    // });
 }
 
 export { addUserToDatabase }
