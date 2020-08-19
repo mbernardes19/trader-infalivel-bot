c513be26e6a0c45392cc3f5969bd6736dd5dd34d
mbernardes19 <bernardes.matheus@outlook.com>
2020-08-19 01:37:44 -0300

    Updates cache service

--- src/services/cache.ts
+++ src/services/cache.ts
@@ -1,7 +1,91 @@
 import NodeCache from 'node-cache';
+import UserData from '../model/UserData';
 
 export default class CacheService {
     private static cache = new NodeCache();
+    static user: UserData|any = {
+        telegramId: '',
+        discountCouponId: '',
+        username: '',
+        paymentMethod: '',
+        plano: '',
+        fullName: '',
+        phone: '',
+        email: '',
+        dataAssinatura: ''
+    }
+
+    static saveFullName(fullName: string) {
+        this.user.fullName = fullName;
+    }
+
+    static getFullName() {
+        return this.user.fullName;
+    }
+
+    static saveDataAssinatura(dataAssinatura: string) {
+        this.user.dataAssinatura = dataAssinatura;
+    }
+
+    static getDataAssinatura() {
+        return this.user.dataAssinatura;
+    }
+
+    static saveUsername(username: string) {
+        this.user.username = username;
+    }
+
+    static getUsername() {
+        return this.user.username;
+    }
+
+    static getPlano() {
+        return this.user.plano;
+    }
+
+    static savePlano(plano: string) {
+        this.user.plano = plano;
+    }
+
+    static getPhone() {
+        return this.user.phone;
+    }
+
+    static savePhone(phone: string) {
+        this.user.phone = phone;
+    }
+
+    static getEmail() {
+        return this.user.email;
+    }
+
+    static saveEmail(email: string) {
+        this.user.email = email;
+    }
+
+    static getPaymentMethod() {
+        return this.user.paymentMethod;
+    }
+
+    static savePaymentMethod(paymentMethod: string) {
+        this.user.paymentMethod = paymentMethod;
+    }
+
+    static saveDiscountId(discountId: string) {
+        this.user.discountCouponId = discountId;
+    }
+
+    static getDiscountId() {
+        return this.user.discountCouponId;
+    }
+
+    static saveTelegramId(telegramId: string) {
+        this.user.telegramId = name;
+    }
+
+    static getTelegramId() {
+        return this.user.telegramId;
+    }
 
     static saveUserData(key: string, value: any) {
         this.save(key, value);
@@ -20,6 +104,7 @@ export default class CacheService {
     }
 
     static clearAllUserData() {
+        this.user = {};
         this.cache.flushAll();
     }
 
