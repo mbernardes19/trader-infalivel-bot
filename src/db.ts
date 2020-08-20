65fd0e44aea5a406d3c57806d200e62cdc5dca1c
mbernardes19 <bernardes.matheus@outlook.com>
2020-08-20 08:33:57 -0300

    Teste

--- src/db.ts
+++ src/db.ts
@@ -2,6 +2,11 @@ import { Connection, createConnection } from 'mysql';
 
 let connection: Connection;
 
+console.log(process.env.DB_HOST)
+console.log(process.env.DB_DATABASE)
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASSWORD)
+
 if (process.env.NODE_ENV === 'production') {
     connection = createConnection({
         host: process.env.DB_HOST,
