196e7d56430a3bb097007bc7278f2024e5bb1aa3
mbernardes19 <bernardes.matheus@outlook.com>
2020-08-22 18:55:37 -0300

    Adds logs to Chat Resolver Service

--- src/services/chatResolver.ts
+++ src/services/chatResolver.ts
@@ -1,10 +1,11 @@
-import { getMonetizzeProductTransaction } from './request';
 import { isBefore } from 'date-fns'
 import { Planos } from '../model/Planos';
+import { log, logError } from '../logger';
 
 export async function getChat(plano: string, dataAssinatura: string): Promise<[string, string]> {
     let chat;
     let chatName;
+    log(`💬 Pegando para plano ${plano} feito na data ${dataAssinatura}`)
     if (process.env.NODE_ENV === 'production') {
         switch(plano) {
             case Planos.SILVER:
@@ -49,25 +50,38 @@ export async function getChat(plano: string, dataAssinatura: string): Promise<[s
         }
     }
 
-    if (process.env.NODE_ENV === 'production') {
-        if (checkIfIsBefore(dataAssinatura, new Date(2020,7,6))) {
-            return ['Canal Gold', process.env.ID_CANAL_GOLD]
+    try {
+        if (process.env.NODE_ENV === 'production') {
+            if (checkIfIsBefore(dataAssinatura, new Date(2020,7,6))) {
+                log(`Compra feita antes de 06/08. Enviando canal Gold.`)
+                return ['Canal Gold', process.env.ID_CANAL_GOLD]
+            } else {
+                log(`Compra feita depois de 06/08. Enviando canal correspondente ${chat}.`)
+                return [chatName, chat]
+            }
         } else {
-            return [chatName, chat]
+            if (checkIfIsBefore(dataAssinatura, new Date(2020,7,6))) {
+                log(`Compra feita antes de 06/08. Enviando canal Gold.`)
+                return ['Canal Gold', process.env.ID_CANAL_TEST_GOLD]
+            } else {
+                log(`Compra feita depois de 06/08. Enviando canal correspondente ${chat}.`)
+                return [chatName, chat]
+            }
         }
-    } else {
-        if (checkIfIsBefore(dataAssinatura, new Date(2020,7,6))) {
-            return ['Canal Gold', process.env.ID_CANAL_TEST_GOLD]
-        } else {
-            return [chatName, chat]
-        }
+    } catch (err) {
+        throw err
     }
 }
 
 const checkIfIsBefore = (data1: string, data2:Date) => {
-    const ano = parseInt(data1.substring(0,4), 10)
-    const mes = parseInt(data1.substring(5,7),10)
-    const dia = parseInt(data1.substring(8,10),10)
-    const result = isBefore(new Date(ano, mes-1, dia), data2)
-    return result;
+    try {
+        const ano = parseInt(data1.substring(0,4), 10)
+        const mes = parseInt(data1.substring(5,7),10)
+        const dia = parseInt(data1.substring(8,10),10)
+        const result = isBefore(new Date(ano, mes-1, dia), data2)
+        return result;
+    } catch (err) {
+        logError(`ERRO AO CHECAR SE COMPRA DE ${data1} FOI FEITA ANTES DE ${data2.toString()}`, err);
+        throw err;
+    }
 }
