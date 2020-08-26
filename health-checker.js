ac5712b8bc9495a6f5a8500c39ed32a8991b603b
mbernardes19 <bernardes.matheus@outlook.com>
2020-08-26 19:12:34 -0300

    Adds health checker

---
+++ health-checker.js
@@ -0,0 +1,24 @@
+const app = require('express')()
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+async function runDeploy() {
+    try {
+        const {stdout, stderr} = await exec('cd .. && cd trader-infalivel-bot && bash ../../restart.sh')
+        console.log('stdout', stdout)
+        console.log('stderr', stderr)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+app.get('/', (req, res) => {
+    res.send('Hello').status(200)
+})
+app.get('/revive', async (req, res) => {
+    await runDeploy()
+    res.sendStatus(200);
+})
+
+const PORT = process.env.PORT_METODO_SEMPRERICO_BOT_HEALTH_CHECKER || process.env.PORT_APP || 21563 || 3000
+app.listen(PORT, () => console.log(`Health checker rodando na porta ${PORT}`));
